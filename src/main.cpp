ee64995175189e8aeacf066f01e7d65d13c80db4
Nathan wittstock <nate@fardog.io>
2020-09-15 22:02:17 -0700

    reformat, improve logging

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
 #include <SD_ZH03B.h>
 #include <SoftwareSerial.h>
 
-
 char sensor_name[40] = "anton-default";
 char influxdb_server[100] = "influxdb";
 char influxdb_port[6] = "8086";
@@ -22,67 +21,83 @@ char influxdb_url[200] = "";
 
 bool configured = false;
 bool should_save_config = false;
-static int NUM_SAMPLES = 10;
-static int MIN_SAMPLES_FOR_SUCCESS = 4;
-static int SAMPLE_DELAY = 3000;
-static int MEASUREMENT_DELAY = 30000;
-static char* URL_TEMPLATE = "http://%s:%s/write?db=%s";
-static char* MEASUREMENT_TEMPLATE = "particulate_matter,node=%s p1_0=%d,p2_5=%d,p10_0=%d";
+
+static const int NUM_SAMPLES = 10;
+static const int MIN_SAMPLES_FOR_SUCCESS = 4;
+static const int SAMPLE_DELAY = 3000;
+static const int MEASUREMENT_DELAY = 30000;
+static const int MAX_JSON_DOCUMENT_SIZE = 2048;
+static const char *URL_TEMPLATE = "http://%s:%s/write?db=%s";
+static const char *MEASUREMENT_TEMPLATE = "particulate_matter,node=%s p1_0=%d,p2_5=%d,p10_0=%d";
 
 SoftwareSerial ZHSerial(D1, D2); // RX, TX
-SD_ZH03B ZH03B( ZHSerial );
+SD_ZH03B ZH03B(ZHSerial);
 
-void save_config_callback () {
-  Serial.println("Should save config");
+void save_config_callback()
+{
+  Serial.println("config: should save config");
   should_save_config = true;
 }
 
-void reset_all() {
+void reset_all()
+{
   WiFi.disconnect();
   SPIFFS.format();
 }
 
-void read_config() {
-  //read configuration from FS json
-  Serial.println("mounting FS...");
+void read_config()
+{
+  Serial.println("config: mounting file system");
 
-  if (SPIFFS.begin()) {
-    Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
-      Serial.println("reading config file");
+  if (SPIFFS.begin())
+  {
+    Serial.println("config: file system mounted");
+    if (SPIFFS.exists("/config.json"))
+    {
+      Serial.println("config: file exists");
+
       File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile) {
-        Serial.println("opened config file");
-        DynamicJsonDocument doc(2048);
+      if (configFile)
+      {
+        Serial.println("config: opened file");
+
+        DynamicJsonDocument doc(MAX_JSON_DOCUMENT_SIZE);
+
         deserializeJson(doc, configFile);
-        // print to output
-        serializeJson(doc, Serial);
-        if (!doc.isNull()) {
-          Serial.println("\nparsed json");
+
+        if (!doc.isNull())
+        {
+          Serial.print("config: parsed json: ");
+          serializeJson(doc, Serial);
+          Serial.print("\n");
 
           strcpy(sensor_name, doc["sensor_name"]);
           strcpy(influxdb_server, doc["influxdb_server"]);
           strcpy(influxdb_port, doc["influxdb_port"]);
           strcpy(influxdb_database, doc["influxdb_database"]);
-          configured = true;
 
-        } else {
-          Serial.println("failed to load json config");
+          configured = true;
         }
+        else
+        {
+          Serial.println("config: failed to load json");
+        }
+
         configFile.close();
       }
     }
-  } else {
-    Serial.println("failed to mount FS");
   }
-  //end read
+  else
+  {
+    Serial.println("config: failed to mount file system");
+  }
 }
 
-void save_config() {
-  Serial.println("saving config");
+void save_config()
+{
+  Serial.println("config: saving config");
 
-  DynamicJsonDocument doc(2048);
+  DynamicJsonDocument doc(MAX_JSON_DOCUMENT_SIZE);
 
   doc["sensor_name"] = sensor_name;
   doc["influxdb_server"] = influxdb_server;
@@ -90,8 +105,9 @@ void save_config() {
   doc["influxdb_database"] = influxdb_database;
 
   File configFile = SPIFFS.open("/config.json", "w");
-  if (!configFile) {
-    Serial.println("failed to open config file for writing");
+  if (!configFile)
+  {
+    Serial.println("config: failed to open file for writing");
     return;
   }
 
@@ -121,7 +137,7 @@ void setup()
   wifiManager.addParameter(&influxdb_database_param);
 
   wifiManager.autoConnect("anton-setup");
-  Serial.println("Connected!");
+  Serial.println("setup: wifi connected");
 
   //read updated parameters
   strcpy(sensor_name, sensor_name_param.getValue());
@@ -131,40 +147,41 @@ void setup()
 
   // create influxdb url
   sprintf(influxdb_url, URL_TEMPLATE, influxdb_server, influxdb_port, influxdb_database);
-  Serial.print("Configured for influxdb url: ");
-  Serial.println(influxdb_url);
+  Serial.printf("setup: influxdb url: %s\n", influxdb_url);
 
-  if (should_save_config) {
+  if (should_save_config)
+  {
     save_config();
   }
 
+  // set up ZH03B sensor
   ZHSerial.begin(9600);
   delay(100);
-  ZH03B.setMode( SD_ZH03B::IU_MODE );
-  Serial.println("-- Reading ZH03B --");
+  ZH03B.setMode(SD_ZH03B::IU_MODE);
   delay(200);
 
   if (ZH03B.sleep())
-    Serial.println("Sleep mode confirmed");
+  {
+    Serial.println("setup: sleep confirmed");
+  }
 }
 
-bool read_sensor_data(uint16_t* arr) {
-  Serial.println("sampling");
-  if( ZH03B.readData() ) {
-    // char printbuf1[80];
-    // Serial.print( ZH03B.getMode() == SD_ZH03B::IU_MODE ? "IU:" : "Q&A:" );  
-    // sprintf(printbuf1, "PM1.0, PM2.5, PM10=[%d %d %d]", ZH03B.getPM1_0(), ZH03B.getPM2_5(), ZH03B.getPM10_0() );
-    // Serial.println(printbuf1);
-
+bool read_sensor_data(uint16_t *arr)
+{
+  Serial.println("sensor: sampling");
+  if (ZH03B.readData())
+  {
     arr[0] = ZH03B.getPM1_0();
     arr[1] = ZH03B.getPM2_5();
     arr[2] = ZH03B.getPM10_0();
 
     return true;
-  } else {   
-    Serial.println( "ZH03B Error reading stream or Check Sum Error" );
+  }
+  else
+  {
+    Serial.println("sensor: Error reading stream or Check Sum Error");
     return false;
-  } 
+  }
 }
 
 int sort_uint16_desc(const void *cmp1, const void *cmp2)
@@ -174,14 +191,16 @@ int sort_uint16_desc(const void *cmp1, const void *cmp2)
   return b - a;
 }
 
-uint16_t median_value(uint16_t* values, int count) {
+uint16_t median_value(uint16_t *values, int count)
+{
   qsort(values, count, sizeof(values[0]), sort_uint16_desc);
 
   return values[(count / 2) - 1];
 }
 
-bool sample_sensor(int* arr) {
-  Serial.println("attemping average sample");
+bool sample_sensor(int *arr)
+{
+  Serial.println("sensor: attemping average sample");
   uint16_t pm1_0[NUM_SAMPLES];
   uint16_t pm2_5[NUM_SAMPLES];
   uint16_t pm10_0[NUM_SAMPLES];
@@ -189,26 +208,30 @@ bool sample_sensor(int* arr) {
 
   uint16_t buf[3];
 
-  for (int i = 0; i < NUM_SAMPLES; i++) {
-    if (read_sensor_data(buf)) {
-      if (buf[0] == buf[1] == buf[2] && buf[0] > 500) {
-        Serial.println("faulty measurement, all values are equal and very large. discarding");
-      } else {
+  for (int i = 0; i < NUM_SAMPLES; i++)
+  {
+    if (read_sensor_data(buf))
+    {
+      if (buf[0] == buf[1] == buf[2] && buf[0] > 500)
+      {
+        Serial.println("sensor: faulty measurement, all values are equal and very large. discarding");
+      }
+      else
+      {
         pm1_0[successes] = buf[0];
         pm2_5[successes] = buf[1];
         pm10_0[successes] = buf[2];
         successes++;
 
-        char printbuf[80];
-        sprintf(printbuf, "Sample: PM1.0, PM2.5, PM10=[%d %d %d]", buf[0], buf[1], buf[2]);
-        Serial.println(printbuf);
+        Serial.printf("sensor: sample PM1.0, PM2.5, PM10=[%d, %d, %d]\n", buf[0], buf[1], buf[2]);
       }
     }
     delay(SAMPLE_DELAY);
   }
 
-  if (successes < MIN_SAMPLES_FOR_SUCCESS) {
-    Serial.println("failed to get minimum number of samples");
+  if (successes < MIN_SAMPLES_FOR_SUCCESS)
+  {
+    Serial.println("sensor: failed to get minimum number of samples");
     return false;
   }
 
@@ -219,20 +242,24 @@ bool sample_sensor(int* arr) {
   return true;
 }
 
-bool post_measurement(int* values) {
+bool post_measurement(int *values)
+{
   bool success = false;
   HTTPClient http;
   http.begin(influxdb_url);
 
   char measurement[100];
   sprintf(measurement, MEASUREMENT_TEMPLATE, sensor_name, values[0], values[1], values[2]);
-  Serial.println(measurement);
+  Serial.printf("influxdb: %s\n", measurement);
 
   int httpCode = http.POST(measurement);
-  if (httpCode == HTTP_CODE_NO_CONTENT) {
+  if (httpCode == HTTP_CODE_NO_CONTENT)
+  {
     success = true;
-  } else {
-    Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+  }
+  else
+  {
+    Serial.printf("influxdb: failed with status: %d\n", httpCode);
   }
 
   http.end();
@@ -242,34 +269,42 @@ bool post_measurement(int* values) {
 
 void loop()
 {
-  if (!configured) {
-    Serial.println("unconfigured, resetting");
+  if (!configured)
+  {
+    Serial.println("loop: unconfigured, resetting");
     reset_all();
     delay(5000);
     return;
   }
 
   if (ZH03B.wakeup())
-    Serial.println("Woke up successfully");
+  {
+    Serial.println("loop: sensor wakeup successfully");
+  }
 
   int sample[3];
   bool success = sample_sensor(sample);
-  if (success) {
-    char printbuf[80];
-    sprintf(printbuf, "Aggregate: PM1.0, PM2.5, PM10=[%d %d %d]", sample[0], sample[1], sample[2]);
-    Serial.println(printbuf);
-  } else {
-    Serial.println("failed to sample sensor");
+  if (success)
+  {
+    Serial.printf("Aggregate: PM1.0, PM2.5, PM10=[%d %d %d]\n", sample[0], sample[1], sample[2]);
+  }
+  else
+  {
+    Serial.println("loop: failed to sample sensor");
   }
 
   if (ZH03B.sleep())
-    Serial.println("Sleep mode confirmed");
+    Serial.println("loop: sensor sleep successful");
 
-  if (success) {
-    if (post_measurement(sample)) {
-      Serial.println("successfully submitted sample");
-    } else {
-      Serial.println("failed to submit sample");
+  if (success)
+  {
+    if (post_measurement(sample))
+    {
+      Serial.println("loop: sample submitted successfully");
+    }
+    else
+    {
+      Serial.println("loop: failed to submit sample");
     }
   }
 

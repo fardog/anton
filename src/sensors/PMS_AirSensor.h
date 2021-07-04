b864f23ef34c36fc2a73a8c3c5bea14449984858
Nathan Wittstock <nate@fardog.io>
2021-07-04 16:31:34 -0700

    add pms7003 particulate sensor

---
+++ src/sensors/PMS_AirSensor.h
@@ -0,0 +1,25 @@
+#ifndef PMS_AirSensor_h
+#define PMS_AirSensor_h
+
+#include <PMS.h>
+#include <SoftwareSerial.h>
+
+#include "AirSensor.h"
+
+class PMS_AirSensor : public AirSensor
+{
+public:
+	PMS_AirSensor(Stream &serial, bool debug = false);
+	~PMS_AirSensor();
+
+	bool getAirData(AirData *data);
+	bool wake();
+	bool sleep();
+
+private:
+	PMS _sensor;
+	PMS::DATA _buf;
+	bool _debug;
+};
+
+#endif
